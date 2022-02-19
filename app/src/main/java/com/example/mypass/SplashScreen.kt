adb8a72c493bbbf16cd8393ebc0eb68cd2ac6216
Artorius81 <91239017+Artorius81@users.noreply.github.com>
2022-02-19 21:32:20 +1000

    Created repository

---
+++ app/src/main/java/com/example/mypass/SplashScreen.kt
@@ -0,0 +1,28 @@
+package com.example.mypass
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        // SplashScreen в полный экран
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // Метод для задержки сплэша
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 в миллисекундах
+    }
+}
