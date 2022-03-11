e63fbed50cb356bab1fdc92dea664f5e3d0afcad
Artorius81 <91239017+Artorius81@users.noreply.github.com>
2022-03-11 17:36:28 +1000

    Added Bottom Navigation

--- app/src/main/java/com/example/mypass/MainPage.kt
+++ app/src/main/java/com/example/mypass/MainPage.kt
@@ -2,8 +2,16 @@ package com.example.mypass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
+import android.widget.Toolbar
+import androidx.navigation.ActivityNavigator
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.mypass.databinding.ActivityMainPageBinding
 
 class MainPage : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
